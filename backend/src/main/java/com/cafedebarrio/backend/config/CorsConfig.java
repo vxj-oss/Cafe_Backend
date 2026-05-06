6d2af93f7ff9dc72bc3ead2ad15ad462ca697604
vxj-oss <vjnontol@ucvvirtual.edu.pe>
2026-05-06 10:14:00 -0500

    Modificacion

--- backend/src/main/java/com/cafedebarrio/backend/config/CorsConfig.java
+++ backend/src/main/java/com/cafedebarrio/backend/config/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("*"
+                            /*"http://localhost:4200"/* */)
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE","OPTIONS")
                         .allowedHeaders("*");
             }
