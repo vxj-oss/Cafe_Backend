f6e9228c6f10f7cf59d068bc9b17ec7b5c96a3db
vxj-oss <vjnontol@ucvvirtual.edu.pe>
2026-05-06 11:11:00 -0500

    Rutas

--- backend/src/main/java/com/cafedebarrio/backend/config/CorsConfig.java
+++ backend/src/main/java/com/cafedebarrio/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("*"
+                        .allowedOrigins("https://front-cafe-pnmm.vercel.app"
                             /*"http://localhost:4200"/* */)
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE","OPTIONS")
                         .allowedHeaders("*");
